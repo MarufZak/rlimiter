e92db6c51c91d26269711fa19c92f8312eda830a
Ma'ruf <marufzak04@gmail.com>
2026-02-12 07:59:16 +0500

    basic tocket bucket

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log("Hello World!")
+interface TocketBucketOpts {
+  maxTokens: number;
+  refillRate: number;
+}
+
+type MapType = Map<
+  string,
+  {
+    count: number;
+    lastTimestamp: number;
+  }
+>;
+
+class TokenBucket {
+  maxTokens = 0;
+  refillRate = 0;
+  map: MapType = new Map();
+
+  constructor({ maxTokens, refillRate }: TocketBucketOpts) {
+    this.maxTokens = maxTokens;
+    this.refillRate = refillRate;
+  }
+
+  consume(ip: string) {
+    const item = this.map.get(ip);
+    const currentTimestamp = new Date().getTime();
+
+    if (!item) {
+      this.map.set(ip, {
+        count: 1,
+        lastTimestamp: currentTimestamp,
+      });
+
+      return true;
+    }
+
+    const { count, lastTimestamp } = item;
+
+    this.map.set(ip, {
+      count: count + 1,
+      lastTimestamp: currentTimestamp,
+    });
+
+    return count < this.maxTokens;
+  }
+}
