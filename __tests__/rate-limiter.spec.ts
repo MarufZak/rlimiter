a78979ab9de3c7e4ffd5604f9c82238985c074fb
Ma'ruf <marufzak04@gmail.com>
2026-02-14 08:53:48 +0500

    tests

--- __tests__/rate-limiter.spec.ts
+++ __tests__/rate-limiter.spec.ts
@@ -1,14 +1,11 @@
 import { type RedisClientType } from 'redis';
 import RateLimiter from '../src';
-import { createRedisClient } from './utils';
+import { createRedisClient, wait } from './utils';
 
 let redisClient: RedisClientType;
 
 beforeAll(async () => {
   const { client } = await createRedisClient();
-
-  if (!client) {
-  }
 
   redisClient = client;
 });
@@ -72,5 +69,59 @@ describe('rate-limiter', () => {
     expect(response1).toBe(true);
     expect(response2).toBe(true);
     expect(response3).toBe(false);
+  });
+
+  it('token refill works correctly', async () => {
+    const limiter = new RateLimiter({
+      maxTokens: 1,
+      refillSeconds: 1,
+      redisClient,
+    });
+
+    const key = `user-1`;
+
+    const response1 = await limiter.check(key);
+    const response2 = await limiter.check(key);
+    expect(response1).toBe(true);
+    expect(response2).toBe(false);
+
+    await wait(1000);
+
+    const response3 = await limiter.check(key);
+    const response4 = await limiter.check(key);
+    expect(response3).toBe(true);
+    expect(response4).toBe(false);
+  });
+
+  it('partial token consuption', async () => {
+    const limiter = new RateLimiter({
+      maxTokens: 3,
+      refillSeconds: 1,
+      redisClient,
+    });
+
+    const key = `user-1`;
+
+    const responses = await Promise.all([
+      limiter.check(key),
+      limiter.check(key),
+    ]);
+
+    expect(responses).toEqual([true, true]);
+
+    await wait(1000);
+
+    const responses2 = await Promise.all([
+      limiter.check(key),
+      limiter.check(key),
+      limiter.check(key),
+      limiter.check(key),
+    ]);
+
+    const success = responses2.filter(response => response === true);
+    const fail = responses2.filter(response => response === false);
+
+    expect(success.length).toBe(3);
+    expect(fail.length).toBe(1);
   });
 });
